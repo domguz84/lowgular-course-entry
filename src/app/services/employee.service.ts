bd14a85373f46849bc24cba10b6f5e50ce49cb0f
domguz84 <d.guzek84@gmail.com>
2022-11-22 23:54:33 +0100

    Lekcja 3 zadanie 5 - Response

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, pipe} from 'rxjs';
 import { map } from 'rxjs/operators';
 import { PersonModel } from '../model/person.model';
 import { CreateEmployeeModel } from '../model/create-employee.model';
+import {ApiResponse} from "./api.response";
+import {EmployeeResponse} from "./employee.response";
 
 @Injectable()
 export class EmployeeService {
@@ -11,9 +13,20 @@ export class EmployeeService {
   }
 
   getAll(): Observable<PersonModel[]> {
-    return this._httpClient.get<PersonModel[]>('assets/data/people.json')
-  }
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees',).pipe(
+      map((response: ApiResponse<EmployeeResponse[]>): PersonModel[] => {
+        return response.data.map((employeeResponse: EmployeeResponse) => {
+          return {
+            name: employeeResponse.employee_name,
+            personalNumber: employeeResponse.id,
+            img: employeeResponse.profile_image,
+            surname: '',
+            mail: employeeResponse.employee_name + "lowgular.io"
+          }
+        });
+      }));
 
+  }
   create(employee: CreateEmployeeModel): Observable<void> {
     return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee).pipe(map( _ => void 0));
   }
